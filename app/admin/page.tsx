c62ac39d383ee8672683b186364caa2f4294025d
Berke Güleryüz <berkeguleryuz@gmail.com>
2025-04-21 17:16:53 +0200

    update

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,8 +1,20 @@
+"use client";
 import { Header } from "@/components/Header";
 import NFTRaffleAdminSetup from "@/components/raffles/NFTRaffleAdminSetup";
 import React from "react";
-
+import { useAccount } from "wagmi";
+import NotFound from "../not-found";
 const AdminPage = () => {
+  const { address } = useAccount();
+  const authorizedAddress = [
+    "0x45938318f986bE73B1f630d2a7F809EFc5672C57",
+    "0x5558F7a27f7eEDD9462764fc8db665F1A6ef6796",
+  ];
+
+  if (!authorizedAddress.includes(address as string)) {
+    return <NotFound />;
+  }
+
   return (
     <div>
       <Header />
